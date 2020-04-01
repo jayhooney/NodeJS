08ba445311dd1691432e42a831444e465b687e7f
hooney731@gmail.com <hooney731@naver.com>
2020-04-01 13:52:45 +0900

    Logutil 간단 사용법 설명 추가

--- Winston/LogUtil.ts
+++ Winston/LogUtil.ts
@@ -1,8 +1,8 @@
-import { createLogger, format, transports } from 'winston';
-import WinstonDaily from 'winston-daily-rotate-file';
-import Path from 'path';
-import Moment from 'moment';
-Moment().utcOffset('+09:00');
+import { createLogger, format, transports } from "winston";
+import WinstonDaily from "winston-daily-rotate-file";
+import Path from "path";
+import Moment from "moment";
+Moment().utcOffset("+09:00");
 
 export class LogUtil {
   private static instance: LogUtil;
@@ -16,7 +16,7 @@ export class LogUtil {
   }
 
   private logDir: string = `${Path.basename(process.mainModule.filename)
-    .replace('.js', '')
+    .replace(".js", "")
     .toUpperCase()}_LOG`;
   private mNodeEnv = process.env.NODE_ENV;
 
@@ -24,38 +24,45 @@ export class LogUtil {
     createLogger({
       // 공통 옵션
       format: format.combine(
-        format((info) => {
-          info.level = info.level.toUpperCase().padStart(5, ' ');
+        format(info => {
+          info.level = info.level.toUpperCase().padStart(5, " ");
           return info;
         })(),
         format.colorize({
-          all: true,
+          all: true
         }),
         format.label({
           label: `${Path.basename(_moduleName)
-            .replace('.js', '')
-            .padStart(12, ' ')}`,
+            .replace(".js", "")
+            .padStart(12, " ")}`
         }),
         format.timestamp({
-          format: 'HH:mm:ss',
+          format: "HH:mm:ss"
         }),
-        format.printf((info) => `| ${info.level} | ${info.timestamp} | ${info.label} >>> ${info.message} `),
+        format.printf(
+          info =>
+            `| ${info.level} | ${info.timestamp} | ${info.label} >>> ${info.message} `
+        )
       ),
       transports: [
         // 로그 출력 설정
         new transports.Console({
-          level: this.mNodeEnv === 'development' ? 'debug' : 'info',
+          level: this.mNodeEnv === "development" ? "debug" : "info"
         }),
         // 로그 파일 설정
         new WinstonDaily({
           format: format.uncolorize(),
-          level: 'debug',
+          level: "debug",
           filename: `${this.logDir}/${Path.basename(process.mainModule.filename)
-            .replace('.js', '')
+            .replace(".js", "")
             .toUpperCase()}_%DATE%.log`,
-          datePattern: 'YYYY-MM-DD',
-          maxFiles: '30d',
-        }),
-      ],
+          datePattern: "YYYY-MM-DD",
+          maxFiles: "30d"
+        })
+      ]
     });
 }
+
+// 사용법
+// import { LogUtil } from 'path/to/LogUtil'
+// const logger = LogUtil.getInstance().logger(__filename)
